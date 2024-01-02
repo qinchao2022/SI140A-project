f548ce27e50375fe2ef6f028886a5348c97bab89
秦朝 <2992265537@qq.com>
2024-01-02 23:02:41 +0800

    validation of correctness of data collected

---
+++ find_distribution.py
@@ -0,0 +1,21 @@
+from scipy.stats import kstest
+from scipy.stats import ks_2samp
+from scipy.stats import entropy
+import numpy as np
+
+def compute_cdf(data):
+    len = data.size
+    cdf = np.zeros(len)
+    cdf[0] = data[0]
+    for i in range(1, len):
+        cdf[i] += cdf[i-1] + data[i]
+    return cdf
+
+def compute_loss(data1, data2):
+    return np.sum(np.square(data1 - data2))
+
+def ks_compare(data1, data2):
+    return ks_2samp(data1, data2).pvalue > ks_2samp(data1, data2).statistic
+
+def KL_divergence(data1, data2):
+    return entropy(data1, data2)
